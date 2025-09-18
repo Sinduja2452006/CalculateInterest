34e5dd6103ad88b74138efb45636e8a8ab6b0e59
Sinduja2452006 <sinduja.senthil24@gmail.com>
2025-09-18 14:19:24 +0530

    initial commit

--- src/com/wipro/bank/main/MainClass.java
+++ src/com/wipro/bank/main/MainClass.java
@@ -1,34 +1,26 @@
 package com.wipro.bank.main;
-
 import com.wipro.bank.service.BankService;
 import java.util.Scanner;
-
 public class MainClass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         BankService service = new BankService();
-
         System.out.print("Enter number of test cases: ");
         int t = sc.nextInt(); 
-
         for (int i = 1; i <= t; i++) {
             System.out.println("\n Test Case " + i );
-
             System.out.print("Enter Tenure (in years): ");
             int tenure = sc.nextInt();
-
             System.out.print("Enter Principal: ");
             float principal = sc.nextFloat();
-
             System.out.print("Enter Age: ");
             int age = sc.nextInt();
-
             System.out.print("Enter Gender: ");
             String gender = sc.next();
-
             service.calculate(principal, tenure, age, gender);
         }
 
         sc.close();
     }
 }
+
